ce0dcaa6432fc52c4d35157b77ec7fedc5115186
Santiago Jaramillo <jara@cshl.edu>
2009-11-28 17:14:55 -0500

    Use signals from dispatcher to prepare new trial.

--- protocoltest.py
+++ protocoltest.py
@@ -15,17 +15,22 @@ from PyQt4 import QtGui
 import numpy as np
 import paramgui
 import dispatcher
+import settings
 
 reload(paramgui)
 reload(dispatcher)
+reload(settings)
 
 
 class Protocol(QtGui.QDialog):
     def __init__(self, parent=None):
         super(Protocol, self).__init__(parent)
 
-        # Add widgets
-        self.dispatcher = dispatcher.Dispatcher(host='soul',connectnow=True)
+        # -- Read settings --
+        smhost = settings.STATE_MACHINE_SERVER
+
+        # -- Add widgets --
+        self.dispatcher = dispatcher.Dispatcher(host=smhost,connectnow=True)
         self.param1 = paramgui.Parameter('OneParam')
 
         layout = QtGui.QVBoxLayout()
@@ -34,13 +39,38 @@ class Protocol(QtGui.QDialog):
         layout.addWidget(self.param1)
         self.setLayout(layout)
         
-        # Set state matrix
-        mat = [ [ 0,  0,  0,  0,  0,  0,  2,  1.2,  0,   0       ] ,\
-                [ 1,  1,  1,  1,  1,  1,  1,   0,   0,   0       ] ,\
-                [ 3,  3,  0,  0,  0,  0,  3,   4,   1,   0       ] ,\
-                [ 2,  2,  0,  0,  0,  0,  2,   4,   2,   0       ] ]
+        # -- Set state matrix --
+        tmin = 0.001            # Very small time
+        Sdur = 0.1              # Duration of sound
+        Tout = 4                # Length of time reward is available
+        Rdur = 0.1              # Duration of reward
+        Lw = settings.LEFT_WATER  # Left water valve
+        Rw = settings.RIGHT_WATER # Right water valve
+        #Corr =  
+        #Err  =
+        mat = []
+        #           Ci  Co  Li  Lo  Ri  Ro  Tout  t  CONTo TRIGo
+        mat.append([ 0,  0,  0,  0,  0,  0,  2, tmin,  0,   0   ]) # 0: State 0
+        mat.append([ 1,  1,  1,  1,  1,  1,  1,   0,   0,   0   ]) # 1: PrepareNextTrial
+        mat.append([ 3,  2,  2,  2,  2,  2,  2,   0,   0,   0   ]) # 2: WaitForCpoke
+        mat.append([ 3,  3,  3,  3,  3,  3,  4, Sdur,  1,   1   ]) # 3: PlayTarget
+        mat.append([ 4,  4,  5,  4,  1,  4,  1, Tout,  0,   0   ]) # 4: WaitForApoke
+        mat.append([ 5,  5,  5,  5,  5,  5,  1, Rdur, Lw,   0   ]) # 5: Reward
         mat = np.array(mat)
+        self.dispatcher.setPrepareNextTrialStates([1,5])
         self.dispatcher.setStateMatrix(mat)
+
+        self.connect(self.dispatcher,QtCore.SIGNAL('PrepareNextTrial'),self.prepareNextTrial)
+        self.connect(self.dispatcher,QtCore.SIGNAL('StartNewTrial'),self.startNewTrial)
+
+
+    def prepareNextTrial(self, nextTrial):
+        print 'Prepare trial %d'%nextTrial
+        self.dispatcher.readyToStartTrial()
+        
+
+    def startNewTrial(self, currentTrial):
+        print 'Started trial %d'%currentTrial
 
 
     def closeEvent(self, event):
@@ -54,6 +84,6 @@ class Protocol(QtGui.QDialog):
 if __name__ == "__main__":
 
     app = QtGui.QApplication(sys.argv)
-    form = Protocol()
-    form.show()
+    protocol = Protocol()
+    protocol.show()
     app.exec_()
