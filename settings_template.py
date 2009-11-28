9220f1f937ed9c01fe062978a8dffcbd54fef55b
Santiago Jaramillo <jara@cshl.edu>
2009-11-28 17:24:56 -0500

    Template of settings file. Make a copy for custom settings.

---
+++ settings_template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+'''
+Rig settings: servers name, data folders, etc.
+
+This file is a template, do not modify it.
+
+You should make a copy named 'settings.py' for each rig and change the
+appropriate settings on that file.
+'''
+
+__version__ = '0.0.1'
+__author__ = 'Santiago Jaramillo <jara@cshl.edu>'
+__created__ = '2009-11-26'
+
+
+STATE_MACHINE_SERVER = '192.168.1.102'
+SOUND_SERVER = STATE_MACHINE_SERVER
+
+DATA_DIR = '/tmp/'
+
+LEFT_WATER  = 1
+RIGHT_WATER = 2
