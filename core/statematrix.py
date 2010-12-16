f3c91a48cfaadf6e92deddcc77f083dfd03309ff
Santiago Jaramillo <sjara@casa>
2010-12-16 00:06:42 -0500

    Fixed printing method, to avoid crashing if there are empty rows.

--- core/statematrix.py
+++ core/statematrix.py
@@ -13,7 +13,7 @@ mat = [ [  0,  0,  0,  0,  0,  0,  2,  1.2,  0,   0   ] ,\
 
 '''
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Santiago Jaramillo <jara@cshl.edu>'
 __created__ = '2009-12-16'
 
@@ -32,12 +32,20 @@ class StateMatrix(object):
     Where the first six are for center, left and right ports, the next
     two columns are for the timer transition and its interval, and the
     last two for digital outputs and sounds.
+
+    FIXME: only one 'readystate' can be specified. It should accept many.
     '''
     def __init__(self,readystate=('ready_next_trial',1)):
-        self.pymat = [[]]
+        self.statesMat = []
         self.statesIndexToName = {}
         self.statesNameToIndex = {}
         self._nextStateInd = 0
+
+        self.schedWavesMat = []
+        self.schedWavesIndexToName = {}
+        self.schedWavesNameToIndex = {}
+        self._nextSchedWaveInd = 0
+
         # FIXME: These should depend on values from smclient
         self.eventsDict = {'Cin':0,'Cout':1,'Lin':2,'Lout':3,
                            'Rin':4,'Rout':5,'Tup':6}
@@ -61,16 +69,22 @@ class StateMatrix(object):
         #StateZeroRow = self._makeDefaultRow(self.readyForNextTrialState)
         #StateZeroRow[IndexOfTimer] = VERYSHORTTIME
         #self._updateStateDict('_STATEZERO',0)
-        JumpStateRow = self._makeDefaultRow(self.readyForNextTrialStateInd)
+        #JumpStateRow = self._makeDefaultRow(self.readyForNextTrialStateInd)
         self._updateStateDict(self.readyForNextTrialStateName,
                               self.readyForNextTrialStateInd)
-        self.pymat.insert(self.readyForNextTrialStateInd,JumpStateRow)
-
+        #self.statesMat.insert(self.readyForNextTrialStateInd,JumpStateRow)
+        self.addState(name=self.readyForNextTrialStateName,selftimer=100)
 
     def _updateStateDict(self,stateName,stateInd):
         '''Add name and index of a state to the dicts keeping the states list.'''
         self.statesNameToIndex[stateName] = stateInd
         self.statesIndexToName[stateInd] = stateName
+
+
+    #def _updateSchedWaveDict(self,stateName,stateInd):
+    #    '''Add name and index of a schedule wave to the dicts keeping the waves list.'''
+    #    self.sxxxxxtatesNameToIndex[stateName] = stateInd
+    #    self.sxxxxxtatesIndexToName[stateInd] = stateName
 
 
     def _appendStateToList(self,stateName):
@@ -79,6 +93,13 @@ class StateMatrix(object):
             self._nextStateInd += 1  # Skip readyForNextTrialState
         self._updateStateDict(stateName,self._nextStateInd)
         self._nextStateInd += 1
+        
+
+    def _appendSchedWaveToList(self,schedWaveName):
+        '''Add schedule wave to the list of available schedule waves.'''        
+        self.schedWavesNameToIndex[schedWaveName] = self._nextSchedWaveInd
+        self.schedWavesIndexToName[self._nextSchedWaveInd] = schedWaveName
+        self._nextSchedWaveInd += 1
         
 
     def addState(self,name='',selftimer=VERYLONGTIME,transitions={},actions={}):
@@ -105,14 +126,48 @@ class StateMatrix(object):
 
         # -- Add row to state transition matrix --
         # IMPROVE: seems very inefficient
-        while len(self.pymat)<(thisStateInd+1):
-            self.pymat.append([])
-        self.pymat[thisStateInd] = NewRow
+        while len(self.statesMat)<(thisStateInd+1):
+            self.statesMat.append([])
+        self.statesMat[thisStateInd] = NewRow
+
+    def addScheduleWave(self, name='',preamble=0, sustain=0, refraction=0, DIOline=-1, soundTrig=0):
+        '''Add a Scheduled Wave to this state machine.
+
+        Example:
+          addScheduleWave(self, name='mySW',preamble=1.2)
+          self.sm.addState(name='first_state', selftimer=100,
+                           transitions={'Cin':'second_state'},
+                           actions={'Dout':LeftLED, 'SchedWaveTrig':'mySW'})
+          self.sm.addState(name='second_state', selftimer=100,
+                           transitions={'mySW_In':'first_state'})
+
+        Note that as currently configured, you can have up to 32
+        different scheduled waves defined per state machine, no more.
+
+        From ExperPort/Modules/@StateMachineAssembler/add_scheduled_wave.m
+
+        '''
+        # FIX ME:
+        # smclient.setScheduledWavesDIO should probably be called only once
+        #  so that it creates the right number of columns
+        # It need to concatenate SWs together
+        # It is dispatcher who sends the matrix to the Server (using smclient)
+
+        ### When to send SWs to Server? probably on self.dispatcher.setStateMatrix()
+
+        # Define inEventCol, outEventCol according to swID
+
+        # -- Find index for this SW (create if necessary) --
+        if name not in self.schedWavesNameToIndex:
+            self._appendSchedWaveToList(name)
+        swID = self.schedWavesNameToIndex[name]
+        schedWaveDefinition = [swID, inEventCol, outEventCol, DIOline, soundTrig,
+                               preamble, sustain, refraction]
 
 
     def getMatrix(self):
         # FIXME: check if there are orphan states or calls to nowhere
-        return self.pymat
+        return self.statesMat
 
 
     def getStatesDict(self,order='NameToIndex'):
@@ -132,9 +187,12 @@ class StateMatrix(object):
     def __str__(self):
         '''String representation of transition matrix.'''
         matstr = ''
-        for (index,onerow) in enumerate(self.pymat):
-            matstr += '%s [%d] \t'%(self.statesIndexToName[index].ljust(16),index)
-            matstr += '\t'.join(str(e) for e in onerow)
+        for (index,onerow) in enumerate(self.statesMat):
+            if len(onerow):
+                matstr += '%s [%d] \t'%(self.statesIndexToName[index].ljust(16),index)
+                matstr += '\t'.join(str(e) for e in onerow)
+            else:
+                matstr += 'EMPTY ROW'
             matstr += '\n'
         return matstr
 
@@ -143,6 +201,7 @@ if __name__ == "__main__":
 
     
     sm = StateMatrix()
+    print sm.statesMat
     '''
     sm.addState(name='wait_for_cpoke', selftimer=10,
                 transitions={'Cin':'play_target'})
@@ -151,9 +210,10 @@ if __name__ == "__main__":
                 actions={'Dout':5})
     sm.addState(name='wait_for_apoke', selftimer=0.5,
                 transitions={'Lout':'wait_for_cpoke','Rout':'wait_for_cpoke'})
-    '''
+
     sm.addState(name='wait_for_cpoke', selftimer=10,
                     transitions={'Cin':'play_target'})
+    print sm.statesMat
     sm.addState(name='play_target', selftimer=1,
                     transitions={'Cout':'wait_for_apoke','Tup':'wait_for_apoke'},
                     actions={'Dout':1})
@@ -169,6 +229,7 @@ if __name__ == "__main__":
 
 
     print(sm)
+    '''
 
     ############# FIX THIS ##########
 
