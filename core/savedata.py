4a56a4a53a292a5215b444c018e7a3eaa8968167
Santiago Jaramillo <sjara@casa.(none)>
2013-07-18 22:32:05 -0400

    Last version before changing to_file() method to require dispatcherModel instead of just the eventsMatrix.

--- core/savedata.py
+++ core/savedata.py
@@ -22,6 +22,7 @@ import h5py
 from PySide import QtCore 
 from PySide import QtGui 
 from taskontrol.settings import rigsettings
+import numpy as np
 
 #class SaveData(QtGui.QWidget):
 class SaveData(QtGui.QGroupBox):
@@ -54,7 +55,15 @@ class SaveData(QtGui.QGroupBox):
         #self.buttonSaveData.clicked.connect(self.fileSave)
 
 
-    def fileSave(self,paramContainer,eventsMatrix):
+    def to_file(self,paramContainer,dispatcherModel):
+        '''
+        Save history of parameters and events to a file.
+        paramContainer must be an instance of paramgui.Container
+        dispatcherModel an instance of dispatcher.Dispatcher
+          which contains the information about events.
+        OLD:eventsMatrix can be created by dispatcher.query_state_machine()
+        '''
+        rawEventsColumnsLabels = ['eventTime','eventCode','nextState']
         thissession=dict()
         thissession['date'] = time.strftime('%Y%m%d',time.localtime())
         thissession['experimenter'] = 'santiago'
@@ -64,37 +73,62 @@ class SaveData(QtGui.QGroupBox):
         fileNameOnly = '%s_%s.%s'%(thissession['subject'],thissession['date'],fileExt)
         defaultFileName = os.path.join(dataDir,fileNameOnly)
 
+        self.logMessage.emit('Saving data...')
+
         # FIXME: what happens if the user presses the CANCEL button?
         if self.checkInteractive.checkState():
-            fname = unicode(QtGui.QFileDialog.getSaveFileName(self,'CHOOSE','/tmp/','*.*'))
+            #fname = unicode(QtGui.QFileDialog.getSaveFileName(self,'CHOOSE','/tmp/','*.*'))
+            fname,ffilter = QtGui.QFileDialog.getSaveFileName(self,'CHOOSE','/tmp/','*.*')
+            if not fname:
+                self.logMessage.emit('Saving cancelled.')
+                return
         else:
             fname = defaultFileName
 
-        self.logMessage.emit('Saving data...')
+        if not len(eventsMatrix):
+            self.logMessage.emit('WARNING: No events have occurred. Nothing was saved.')
+            return
 
         # -- Create data file --
         # FIXME: check that file opened correctly
+        if os.path.exists(fname):
+            print('File exists. I will rewrite {0}'.format(fname))
         h5file = h5py.File(fname,'w')
-        eventsGroup = h5file.create_group('/events') # Events that ocurred during the sessio
-        eventsGroup.create_dataset('rawEvents', dtype=float, data=eventsMatrix)
-        paramContainer.append_to_file(h5file)
+        try:
+            eventsGroup = h5file.create_group('/events') # Events that ocurred during the sessio
+            eventsGroup.create_dataset('rawEvents', dtype=float, data=eventsMatrix)
+            dtstr = h5py.special_dtype(vlen=str)
+            eventsGroup.create_dataset('rawEventsColumnsLabels', dtype=dtstr,
+                                       data=rawEventsColumnsLabels)
+            paramContainer.append_to_file(h5file)
+        except:
+            h5file.close()
+            raise
 
         h5file.close()
 
-        paramContainer.printItems()
+        #paramContainer.print_items()
         self.logMessage.emit('Saved data to %s'%fname)
         #messenger.Messenger.send('Saved data to %s'%fname)
         #messenger.Messenger.send('Saved data to %s'%fname,sender=__name__)
 
 if __name__ == "__main__":
     import signal
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    signal.signal(signal.SIGINT, signal.SIG_DFL) # Enable Ctrl-C
     import sys
-    app = QtGui.QApplication(sys.argv)
+    # -- A workaround to enable re-running the app in ipython after closing --
+    app=QtGui.QApplication.instance() # checks if QApplication already exists 
+    if not app: # create QApplication if it doesnt exist 
+        app = QtGui.QApplication(sys.argv)
     form = QtGui.QDialog()
     saveData = SaveData()
     layoutMain = QtGui.QHBoxLayout()
     layoutMain.addWidget(saveData)
     form.setLayout(layoutMain)
+    def onbutton():
+        import paramgui
+        params = paramgui.Container()
+        saveData.to_file(params,[[0,0,0]])
+    saveData.buttonSaveData.clicked.connect(onbutton)
     form.show()
     app.exec_()
