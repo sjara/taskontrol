ba18dd6e7ce30419a210b5c58f55e01f9b13c111
Santiago Jaramillo <sjara@casa.(none)>
2014-02-09 19:08:20 -0800

    Minor changes.

---
+++ core/__init__.py
@@ -0,0 +1,27 @@
+'''
+savedata
+--------
+.. automodule:: taskontrol.core.savedata
+   :members:
+
+statematrix
+--------
+.. automodule:: taskontrol.core.statematrix
+   :members:
+
+
+paramgui
+--------
+.. automodule:: taskontrol.core.paramgui
+   :members:
+
+
+'''
+
+'''
+NOT READY
+dispatcher
+----------
+.. automodule:: taskontrol.core.dispatcher
+   :members:
+'''
