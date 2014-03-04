0cff2fcbd7a61ccb6e28e3555740febfef465266
Santiago Jaramillo <sjara@eon.(none)>
2014-03-04 08:58:23 -0800

    Remove light stimulus (only sounds now, except in direct modes).

--- examples/example012_fulltask_sound.py
+++ examples/example012_fulltask_sound.py
@@ -280,7 +280,7 @@ class Paradigm(templates.Paradigm2AFC):
                               transitions={'Tup':'playStimulus','Cout':'waitForCenterPoke'})
             self.sm.add_state(name='playStimulus', statetimer=targetDuration,
                               transitions={'Tup':'waitForSidePoke','Cout':'earlyWithdrawal'},
-                              outputsOn=[stimOutput],serialOut=soundID)
+                              serialOut=soundID) #outputsOn=[stimOutput],
             self.sm.add_state(name='waitForSidePoke', statetimer=rewardAvailability,
                               transitions={'Lin':'choiceLeft','Rin':'choiceRight',
                                            'Tup':'noChoice'},
@@ -322,7 +322,7 @@ class Paradigm(templates.Paradigm2AFC):
                               transitions={'Tup':'playStimulus','Cout':'waitForCenterPoke'})
             self.sm.add_state(name='playStimulus', statetimer=targetDuration,
                               transitions={'Tup':'waitForSidePoke','Cout':'earlyWithdrawal'},
-                              outputsOn=[stimOutput],serialOut=soundID)
+                              serialOut=soundID) #outputsOn=[stimOutput],
             self.sm.add_state(name='waitForSidePoke', statetimer=rewardAvailability,
                               transitions={'Lin':'choiceLeft','Rin':'choiceRight',
                                            'Tup':'noChoice'},
