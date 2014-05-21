35df1b08d53386d394a7c5faa9a8e3d9570d46a1
JaraLab User <jarauser@jararig4.(none)>
2014-05-21 10:05:08 -0700

    Changed new mice to direct mode

--- examples/params.py
+++ examples/params.py
@@ -16,33 +16,33 @@ sidesDirectMode = {'outcomeMode':'sides_direct', 'delayToTargetMean':0, 'delayTo
 directMode = {'outcomeMode':'direct', 'delayToTargetMean':0, 'delayToTargetHalfRange':0,
                    'currentBlock':'mid_boundary'}
 
-test050 = sidesDirectMode.copy()
+test050 = directMode.copy()
 test050.update({'subject':'test050','experimenter':'santiago'})
 
-test051 = sidesDirectMode.copy()
+test051 = directMode.copy()
 test051.update({'subject':'test051','experimenter':'santiago'})
 
-test052 = sidesDirectMode.copy()
+test052 = directMode.copy()
 test052.update({'subject':'test052','experimenter':'santiago'})
 
-test053 = sidesDirectMode.copy()
+test053 = directMode.copy()
 test053.update({'subject':'test053','experimenter':'santiago'})
 
-test054 = sidesDirectMode.copy()
+test054 = directMode.copy()
 test054.update({'subject':'test054','experimenter':'santiago'})
 
-test055 = sidesDirectMode.copy()
+test055 = directMode.copy()
 test055.update({'subject':'test055','experimenter':'santiago'})
 
-test056 = sidesDirectMode.copy()
+test056 = directMode.copy()
 test056.update({'subject':'test056','experimenter':'santiago'})
 
-test057 = sidesDirectMode.copy()
+test057 = directMode.copy()
 test057.update({'subject':'test057','experimenter':'santiago'})
 
-test058 = sidesDirectMode.copy()
+test058 = directMode.copy()
 test058.update({'subject':'test058','experimenter':'santiago'})
 
-test059 = sidesDirectMode.copy()
+test059 = directMode.copy()
 test059.update({'subject':'test059','experimenter':'santiago'})
 
