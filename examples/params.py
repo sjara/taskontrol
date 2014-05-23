ee00e64afda71fe79c8efc623e580f6c5d796deb
Santiago Jaramillo <sjara@eon.(none)>
2014-05-23 15:47:28 -0700

    Changed new mice to increase_delay.

--- examples/params.py
+++ examples/params.py
@@ -15,34 +15,36 @@ sidesDirectMode = {'outcomeMode':'sides_direct', 'delayToTargetMean':0, 'delayTo
                    'currentBlock':'mid_boundary'}
 directMode = {'outcomeMode':'direct', 'delayToTargetMean':0, 'delayToTargetHalfRange':0,
                    'currentBlock':'mid_boundary'}
+increaseDelayMode = {'outcomeMode':'on_next_correct', 'delayToTargetMean':0, 'delayToTargetHalfRange':0,
+                   'currentBlock':'mid_boundary', 'automationMode':'increase_delay', 'targetDuration':0.05}
 
-test050 = directMode.copy()
+test050 = increaseDelayMode.copy()
 test050.update({'subject':'test050','experimenter':'santiago'})
 
-test051 = directMode.copy()
+test051 = increaseDelayMode.copy()
 test051.update({'subject':'test051','experimenter':'santiago'})
 
-test052 = directMode.copy()
+test052 = increaseDelayMode.copy()
 test052.update({'subject':'test052','experimenter':'santiago'})
 
-test053 = directMode.copy()
+test053 = increaseDelayMode.copy()
 test053.update({'subject':'test053','experimenter':'santiago'})
 
-test054 = directMode.copy()
+test054 = increaseDelayMode.copy()
 test054.update({'subject':'test054','experimenter':'santiago'})
 
-test055 = directMode.copy()
+test055 = increaseDelayMode.copy()
 test055.update({'subject':'test055','experimenter':'santiago'})
 
-test056 = directMode.copy()
+test056 = increaseDelayMode.copy()
 test056.update({'subject':'test056','experimenter':'santiago'})
 
-test057 = directMode.copy()
+test057 = increaseDelayMode.copy()
 test057.update({'subject':'test057','experimenter':'santiago'})
 
-test058 = directMode.copy()
+test058 = increaseDelayMode.copy()
 test058.update({'subject':'test058','experimenter':'santiago'})
 
-test059 = directMode.copy()
+test059 = increaseDelayMode.copy()
 test059.update({'subject':'test059','experimenter':'santiago'})
 
