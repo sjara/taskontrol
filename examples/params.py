8698602aaf2ddf9636c62b255909d205b1d37c32
Santiago Jaramillo <sjara@eon.(none)>
2014-06-18 21:30:27 -0700

    Changed test019 to only low-freqs.

--- examples/params.py
+++ examples/params.py
@@ -50,7 +50,7 @@ test018 = switchBlocksMode.copy()
 test018.update({'subject':'test018','experimenter':'santiago'})
 
 test019 = switchBlocksMode.copy()
-test019.update({'subject':'test019','experimenter':'santiago'})
+test019.update({'subject':'test019','experimenter':'santiago', 'currentBlock':'low_boundary', 'trialsPerBlock':1000})
 
 test020 = switchBlocksMode.copy()
 test020.update({'subject':'test020','experimenter':'santiago'})
