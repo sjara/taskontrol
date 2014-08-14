0783eb0e3b0556d4ae8cf2e31cd50fef93b3ecc7
Santiago Jaramillo <sjara@uoregon.edu>
2014-08-14 08:57:09 -0700

    Changed test050-059 to switching every day.

--- examples/params.py
+++ examples/params.py
@@ -23,7 +23,8 @@ increaseDelayMode = {'outcomeMode':'on_next_correct', 'delayToTargetMean':0, 'de
 #onNextCorrectMode = {'currentBlock':'mid_boundary','lowFreq':3000,'highFreq':16000}
 #onNextCorrectMode = {'currentBlock':'low_boundary','trialsPerBlock':1000}
 #switchDailyMode = {'trialsPerBlock':2000,'punishTimeError':4}
-switchDailyMode = {'trialsPerBlock':2000,'punishTimeError':4,'currentBlock':'mid_boundary','lowFreq':5000,'midFreq':11000,'highFreq':24000}
+#switchDailyMode = {'trialsPerBlock':2000,'punishTimeError':4,'currentBlock':'mid_boundary','lowFreq':5000,'midFreq':11000,'highFreq':24000}
+switchDailyMode = {'trialsPerBlock':2000,'punishTimeError':4,'lowFreq':5000,'midFreq':11000,'highFreq':24000}
 
 switchBlocksMode = {'punishTimeError':4}
 
