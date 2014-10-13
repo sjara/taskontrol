0baf4f02a993b0a8cc83c54af4bbb13c6356c95f
Santiago Jaramillo <sjara@uoregon.edu>
2014-10-13 11:37:42 -0700

    Added Angie's mice (test064-test069).

--- examples/params.py
+++ examples/params.py
@@ -30,9 +30,30 @@ psyCurveMidBound = {'trialsPerBlock':2000,'punishTimeError':4,'lowFreq':5000,'mi
                     'currentBlock':'mid_boundary','psycurveMode':'uniform'}
 #switchDailyMode = psyCurveMidBound
 
+shapingMode = {'lowFreq':5000,'midFreq':11000,'highFreq':24000, 'currentBlock':'mid_boundary','targetIntensityMode':'fixed'}
 
 
 switchBlocksMode = {'punishTimeError':4}
+
+# -- Angie's mice --
+test069 = shapingMode.copy()
+test069.update({'subject':'test069','experimenter':'santiago'})
+
+test068 = shapingMode.copy()
+test068.update({'subject':'test068','experimenter':'santiago'})
+
+test067 = shapingMode.copy()
+test067.update({'subject':'test067','experimenter':'santiago'})
+
+test066 = shapingMode.copy()
+test066.update({'subject':'test066','experimenter':'santiago'})
+
+test065 = shapingMode.copy()
+test065.update({'subject':'test065','experimenter':'santiago'})
+
+test064 = shapingMode.copy()
+test064.update({'subject':'test064','experimenter':'santiago'})
+
 
 test011 = switchBlocksMode.copy()
 test011.update({'subject':'test011','experimenter':'santiago'})
