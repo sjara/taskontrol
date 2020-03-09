8cd22209d2f8acb038e6d3f8ab9f8d1599160900
Santiago Jaramillo <sjara@uoregon.edu>
2020-03-09 10:16:58 -0700

    Added SingleDrop class.

--- plugins/manualcontrol.py
+++ plugins/manualcontrol.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
-
-'''
+"""
 Plugin for controlling outputs manually.
-
 
 TO DO:
 - The sorting of the buttons is currently alphabetical. Bad idea.
   I will need to sort by the value of OUTPUTS.
-
-
-'''
+"""
 
 __version__ = '0.1.1'
 __author__ = 'Santiago Jaramillo <sjara@uoregon.edu>'
@@ -22,10 +17,9 @@ from taskontrol.settings import rigsettings
 BUTTON_COLORS = {'on':'red','off':'black'}
 
 class ManualControl(QtGui.QGroupBox):
-    '''
-    Manual control of outputs
-    '''
-
+    """
+    Manual control of outputs.
+    """
     def __init__(self, statemachine, parent=None):
         super(ManualControl, self).__init__(parent)
 
@@ -34,7 +28,7 @@ class ManualControl(QtGui.QGroupBox):
         self.outputButtons = {}
         nButtons = 0
         nCols = 2
-        dictIterator = iter(sorted(rigsettings.OUTPUTS.iteritems()))
+        dictIterator = iter(sorted(rigsettings.OUTPUTS.items()))
         for key,value in dictIterator:
             self.outputButtons[key] = OutputButton(statemachine, key,value)
             self.outputButtons[key].setObjectName('ManualControlButton')
@@ -47,18 +41,19 @@ class ManualControl(QtGui.QGroupBox):
         self.setLayout(layout)
         self.setTitle('Manual control')
 
- 
+
 class OutputButton(QtGui.QPushButton):
-    '''Single button for manual output'''
+    """
+    Single button for manual output.
+    """
     def __init__(self, statemachine, buttonText, outputIndex, parent=None):
         super(OutputButton, self).__init__(buttonText, parent)
-
         #self.setMinimumHeight(50)
         self.statemachine = statemachine
         self.outputIndex = outputIndex
         self.setCheckable(True)
-        self.connect(self,QtCore.SIGNAL('clicked()'),self.toggleOutput)
-
+        #self.connect(self,QtCore.SIGNAL('clicked()'),self.toggleOutput) ### OOLD
+        self.clicked.connect(self.toggleOutput)
         #stylestr = 'QPushButton {font: 10pt}'
         #self.setStyleSheet(stylestr)
 
@@ -69,21 +64,19 @@ class OutputButton(QtGui.QPushButton):
             self.stop()
 
     def start(self):
-        '''Start action.'''
         stylestr = 'QPushButton {{color: {0}; font: bold}}'.format(BUTTON_COLORS['on'])
         self.setStyleSheet(stylestr)
         self.statemachine.force_output(self.outputIndex,1)
 
     def stop(self):
-        '''Stop action.'''
         stylestr = ''
         self.setStyleSheet(stylestr)
         self.statemachine.force_output(self.outputIndex,0)
 
 class WaterControl(QtGui.QGroupBox):
-    '''
+    """
     Manual control of water valves
-    '''
+    """
     def __init__(self, statemachine, parent=None):
         super(WaterControl, self).__init__(parent)
         # -- Create graphical objects --
@@ -100,7 +93,51 @@ class WaterControl(QtGui.QGroupBox):
         self.setTitle('Water control')
 
 
+class TimedButton(QtGui.QPushButton):
+    """
+    Single button for timed activation of output.
+    """
+    def __init__(self, statemachine, buttonText, outputIndex, toggleTime=0.03, parent=None):
+        super(TimedButton, self).__init__(buttonText, parent)
+        self.statemachine = statemachine
+        self.outputIndex = outputIndex
+        self.toggleTime = toggleTime
+        self.clicked.connect(self.start)
+        self.timer = QtCore.QTimer(self)
+
+    def start(self):
+        stylestr = 'QPushButton {{color: {0}; font: bold}}'.format(BUTTON_COLORS['on'])
+        self.setStyleSheet(stylestr)
+        self.statemachine.force_output(self.outputIndex,1)
+        self.timer.singleShot(1e3*self.toggleTime, self.stop) # timer takes interval in ms
+
+    def stop(self):
+        stylestr = ''
+        self.setStyleSheet(stylestr)
+        self.statemachine.force_output(self.outputIndex,0)
+
+class SingleDrop(QtGui.QGroupBox):
+    """
+    Buttons to deliver a small drop of water.
+    """
+    def __init__(self, statemachine, toggleTime=0.03, parent=None):
+        super(SingleDrop, self).__init__(parent)
+        # -- Create graphical objects --
+        layout = QtGui.QHBoxLayout()
+        self.outputButtons = {}
+        outputsDict = {'Left':rigsettings.OUTPUTS['leftWater'],
+                       'Right':rigsettings.OUTPUTS['rightWater']}
+        for key,value in iter(sorted(outputsDict.iteritems())):
+            self.outputButtons[key] = TimedButton(statemachine, key, value, toggleTime=toggleTime)
+            self.outputButtons[key].setObjectName('SingleDropButton')
+            self.outputButtons[key].setMinimumHeight(40)
+            layout.addWidget(self.outputButtons[key])
+        self.setLayout(layout)
+        self.setTitle('Single drop')
+
+
 if __name__ == "__main__":
+
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     import sys
@@ -109,11 +146,14 @@ if __name__ == "__main__":
     except RuntimeError:
       app = QtCore.QCoreApplication.instance()
     form = QtGui.QDialog()
-    from taskontrol.plugins import smdummy
-    statemachine = smdummy.StateMachineClient()
+    #from taskontrol.plugins import smdummy as statemachine
+    from taskontrol.plugins import smemulator as statemachine
+    statemachine = statemachine.StateMachineClient()
     mc = ManualControl(statemachine)
+    sdrop = SingleDrop(statemachine, toggleTime=0.5)
     layoutMain = QtGui.QHBoxLayout()
     layoutMain.addWidget(mc)
+    layoutMain.addWidget(sdrop)
     form.setLayout(layoutMain)
     form.show()
     app.exec_()
