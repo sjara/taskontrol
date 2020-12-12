dd2522019b8fa1a2cbd618f4f52e00023ba36916
Santiago Jaramillo <sjara@uoregon.edu>
2020-12-11 21:58:32 -0800

    New version of wheelsensor (Arduino UNO + rotary encoder).

--- taskontrol/plugins/wheelclient.py
+++ taskontrol/plugins/wheelclient.py
@@ -2,12 +2,10 @@
 Client for the rotary encoder server running on an Arduino Uno.
 '''
 
-from __future__ import division
 import sys
 import serial
 import struct
 import time
-import matplotlib.pyplot as plt
 import numpy as np
 
 SERIAL_PORT_PATH = '/dev/ttyACM0'
@@ -29,36 +27,52 @@ opcode = {
     'GET_SAMPLING_FACTOR' : 0x0b,
     'SET_N_PERIODS'       : 0x0c,
     'GET_N_PERIODS'       : 0x0d,
-    'GET_SERVER_VERSION'  : 0x0e,
+    'GET_VERSION'         : 0x0e,
     'GET_TICK_TIMES'      : 0x0f,
     'RUN'                 : 0xee,
     'SET_DEBUG_MODE'      : 0xf0,
     'ERROR'               : 0xff,
 }
-for k,v in opcode.iteritems():
-    opcode[k]=chr(v)
+for k,v in opcode.items():
+    opcode[k]=bytes([v])
 
 
 class WheelClient(object):
-    def __init__(self,connectnow=True):
+    def __init__(self, connectnow=True):
         '''Wheel sensor client for the Arduino Due.'''
         self.ser = serial.Serial(SERIAL_PORT_PATH, SERIAL_BAUD, timeout=SERIAL_TIMEOUT)
         #self.tickTimes = np.array([],dtype=int)
         
     def test_connection(self):
-        self.ser.write(opcode['TEST_CONNECTION'])
-        connectionStatus = self.ser.read()
-        if connectionStatus==opcode['OK']:
-            return 'OK'
-        else:
-            raise IOError('Connection to the wheel sensor Arduino was lost.')
+        """
+        Clear serial buffer and test whether connection to wheel sensor is alive.
+        Returns 'OK' if connection is alive.
+        """
+        print('Testing connection to wheelsensor...', end='', flush=True)
+        self.ser.readlines()
+        for attempt in range(10):
+            self.ser.write(opcode['TEST_CONNECTION'])
+            connectionStatus = self.ser.read()
+            if connectionStatus==opcode['OK']:
+                print(' Ready.')
+                return 'OK'
+            time.sleep(0.01)
+            print('.', end='', flush=True)
+            #print(f'{attempt}: {connectionStatus}')
+        raise IOError('Connection to the wheel sensor Arduino was lost.')
+            
 
     def print_serial(self):
         oneline = 'not-empty'
         while oneline:
             oneline = self.ser.readline()
-            print oneline,
+            print(oneline,end='')
             
+    def get_version(self):
+        self.ser.write(opcode['GET_VERSION'])
+        valueStr = self.ser.readline()
+        return valueStr.strip()
+    
     def set_threshold_move(self, value):
         self.ser.write(opcode['SET_THRESHOLD_MOVE'])
         packedValue = struct.pack('<B',value)
@@ -112,5 +126,14 @@ if __name__ == '__main__':
     wheelclient.run()
     while(1):
         oneline = wheelclient.ser.readline()
-        if oneline:
-            print oneline,
+        #if oneline:
+        #    print(oneline,end='')
+        if oneline.strip():
+            ts, val = [int(x) for x in oneline.strip().split(b' ')]
+            if 1:
+                print(f'{ts}:\t {val}')
+            else:
+                if val>0:
+                    print(val*'+')
+                else:
+                    print(abs(val)*'-')
