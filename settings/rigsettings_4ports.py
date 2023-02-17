310f185020bc6708f7be3b1a53cc59e4a00be585
Santiago Jaramillo <sjara@uoregon.edu>
2023-02-17 14:49:09 -0800

    Added rigsettings for 4 ports.

---
+++ settings/rigsettings_4ports.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+'''
+This script defines the names of inputs and outputs, the type of state
+machine and sound server, the data directories, and other parameters.
+
+*This file is a template, do not modify it.* Make a copy of this file
+and name it 'rigsettings.py'. Modify this new file to define the
+appropriate settings for the rig.
+
+'''
+
+__version__ = '0.2'
+__author__ = 'Santiago Jaramillo <sjara@uoregon.edu>'
+__created__ = '2013-03-18'
+
+
+#: Operating system
+OS = 'ubuntu1804'
+
+#: Type of state machine. Either 'arduino_due' or 'dummy'
+STATE_MACHINE_TYPE = 'arduino_due'
+#STATE_MACHINE_TYPE = 'dummy'
+#STATE_MACHINE_TYPE = 'emulator'
+
+#: Serial port for the state machine.
+STATE_MACHINE_PORT = '/dev/ttyACM0'
+
+#: Parameters for triggering sounds.
+SOUND_TRIGGER_PORT = '/dev/ttyACM1'
+
+#: File that defines the calibration of speakers
+SPEAKER_CALIBRATION_SINE = None
+#SPEAKER_CALIBRATION_SINE = '/home/jarauser/src/taskontrol/settings/speaker_calibration_sine.h5'
+SPEAKER_CALIBRATION_CHORD = None
+#SPEAKER_CALIBRATION_CHORD = '/home/jarauser/src/taskontrol/settings/speaker_calibration_chord.h5'
+SPEAKER_CALIBRATION_NOISE = None
+#SPEAKER_CALIBRATION_NOISE = '/home/jarauser/src/taskontrol/settings/speaker_calibration_noise.h5'
+
+#: Computer volume level [0-100%]
+SOUND_VOLUME_LEVEL = 82
+
+DATA_DIR = '/data/behavior/'
+REMOTE_DIR = '/mnt/jarahubdata/'
+
+DEFAULT_PARAMSFILE = './params.py'
+
+#: Name for each input line.
+INPUTS = {
+    'N1'  :0,
+    'N2'  :1,
+    'S1'  :2,
+    'S2'  :3,
+}
+EMULATOR_GUI_ORDER = [0,1,2,3]
+
+#: Name for each output line.
+OUTPUTS = {
+    'N1Water':0,
+    'N1LED'  :1,
+    'N2Water':2,
+    'N2LED'  :3,
+    'S1Water':4,
+    'S1LED'  :5,
+    'S2Water':6,
+    'S2LED'  :7,
+}
+
+"""
+# -- The following must match the state machine settings --
+INPUTS = {
+    'C'  :0,
+    'L'  :1,
+    'R'  :2,
+}
+#: Name for each output line.
+OUTPUTS = {
+    'centerWater':0,
+    'centerLED'  :1,
+    'leftWater'  :2,
+    'leftLED'    :3,
+    'rightWater' :4,
+    'rightLED'   :5,
+    'stim1'      :6,
+    'stim2'      :7,
+}
+"""
+    
+#: Make the emulator print details
+EMULATOR_VERBOSE = True
+
+#: Settings for sending sync signals through one channel of the sound card
+SOUND_SYNC_CHANNEL = None
+#SOUND_SYNC_CHANNEL = 0
+SYNC_SIGNAL_AMPLITUDE = 0.1
+SYNC_SIGNAL_FREQUENCY = 500
+
